7dbeb52c5d2ef5888d42edf6c2ff505bc6d4569f
mshub111 <salammariam890@gmail.com>
2026-06-02 00:43:50 +0200

    Add simple calculator functions

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
 def hello():
     print("Hello from DevOps 1 project!")
 
 if __name__ == "__main__":
-    hello() 
+    hello()
+    print("2 + 3 =", add(2, 3))
+    print("4 * 5 =", multiply(4, 5))
+    
